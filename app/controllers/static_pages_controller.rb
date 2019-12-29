b27514d947ff0bc877ee4518cea327bcc1c5eb67
Piotr Kędzia <ptrkedzia@gmail.com>
2019-12-29 23:56:25 +0100

    Add static pages specs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
-class StaticPagesController < ApplicationController
-  def home
-  end
+# frozen_string_literal: true
 
-  def help
-  end
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def about; end
+
+  def help; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
   get 'help', to: 'static_pages#help'
+  get 'about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#hello'
