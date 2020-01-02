62d771022094e9a82dd588ca15b81bf0315d6d6c
Piotr Kędzia <ptrkedzia@gmail.com>
2020-01-02 16:53:57 +0100

    Add a `full_title` helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  BASE_TITLE = 'Sample App'
+
+  def full_title(page_title: '')
+    if page_title.empty?
+      BASE_TITLE
+    else
+      "#{page_title} | #{BASE_TITLE}"
+    end
+  end
 end
