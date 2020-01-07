b221789f6135334bbd7007299a1f3e1e6d3956fd
Piotr Kędzia <ptrkedzia@gmail.com>
2020-01-07 11:53:58 +0100

    Configure feature specs

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Static pages' do
+  it ' should render navigation links on the top navbar' do
+    visit root_path
+
+    expect(page).to have_link 'Home'
+    expect(page).to have_link 'Help'
+    expect(page).to have_link 'Log in'
+  end
+
+  describe 'Home page' do
+    it 'should display a home page' do
+      visit home_path
+
+      expect(page).to have_text 'Welcome to the Sample App'
+    end
+  end
+
+  describe 'Help page' do
+    it 'should display a help page' do
+      visit help_path
+
+      expect(page).to have_text 'This is the help page'
+    end
+  end
+
+  describe 'Log in page' do
+    it 'should display a log in page' do
+      visit signup_path
+
+      expect(page).to have_text 'Users#new'
+    end
+  end
+end
